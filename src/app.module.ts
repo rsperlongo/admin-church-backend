e4fe2e7c3245441737b3626598845bb6fd0b3480
Ricardo Tadeu Sperlongo <ricardo_tadeu30@hotmail.com>
2023-08-03 16:46:40 -0300

    update app modules moongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -29,7 +29,9 @@ import { AuthModule } from './auth/auth.module';
         autoLoadEntities: true,
       }),
     }),
-    MongooseModule.forRoot('mongodb://localhost:27017/authentication'),
+    MongooseModule.forRoot('mongodb://localhost:27017/nestjs', {
+      connectionName: 'auth',
+    }),
     UsersModule,
     AuthModule,
   ],
