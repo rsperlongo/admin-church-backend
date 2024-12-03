0b3a07e9407a2b3019ef6664a646bc8f6920c191
Ricardo Tadeu Sperlongo <ricardo_tadeu30@hotmail.com>
2024-12-03 15:19:06 -0300

    events service

--- src/events/events.service.ts
+++ src/events/events.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException, HttpStatus, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { EventsDto } from 'src/@core/domain/dto/events.dto';
 import { EventsEntity } from 'src/@core/domain/entities/events.entity';
@@ -20,5 +20,35 @@ export class EventsService {
 
     async findAll() {
         return this.eventsRepository.find();
+    }
+
+    async findOne(id: string): Promise<EventsEntity[]> {
+        const events = await this.eventsRepository.find({ where: { id}  });
+
+        if(!events) {
+            throw new HttpException(`Member ${id} not found`, HttpStatus.NOT_FOUND)
+        }
+
+        return events;
+    }
+
+    async update(id: string, updateEvents: EventsEntity) {
+        const events = await this.eventsRepository.preload({
+            id;
+            ...updateEvents
+        });
+        if(!events) {
+            throw new HttpException(`Event ${id} not found`, HttpStatus.NOT_FOUND)
+        }
+        return events;
+    }
+
+    async remove(id: string) {
+        const events = await this.eventsRepository.findOne({ where: { id } });
+
+        if(!events) {
+            throw new NotFoundException(`Member ${id} not found`);
+        }
+        return this.eventsRepository.remove(events);
     }
 }
